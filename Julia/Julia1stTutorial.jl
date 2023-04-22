225772adbe082015010fb93ed9bc4eaa0eb84a5b
Fjgl96 <francisco.guerrero@fgconsulting.app>
2023-04-22 04:10:06 -0500

    Julia1st

---
+++ Julia/Julia1stTutorial.jl
@@ -0,0 +1,6 @@
+## Basic Operations with Julia.
+
+### The followgin snippet of code whos the basic operatings that the user can realize
+### by using Julia, with htis the surer can perform some operations, like sums, power,
+### function declaration and many others that could torun out to be usefful for his 
+### purposes
