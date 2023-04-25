f8df096ae6aed92464b685f5dfa957b68c49004d
Fjgl96 <francisco.guerrero@fgconsulting.app>
2023-04-24 22:23:23 -0500

    h

---
+++ R/Ejemplo1.r
@@ -0,0 +1,21 @@
+
+# add (10, 1)
+add <- function(x, y) {
+    x + y
+}
+print(add (1, -2))
+print(add (1.0e10, 2.0e10))
+print(paste ("one", NULL))
+print(paste (NA, 'two'))
+print(paste ("multi-
+line",
+'multi-
+line'))
+
+gaaa <- function(x){
+    print("gaaaaaaa")
+}
+gaaa(5)
+
+
+add2 <- function
